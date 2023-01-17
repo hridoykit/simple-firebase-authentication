650d00a0ec3eb73a7da93bdd93265401e3f57eb0
hridoykit <hridoy.tsa@gmail.com>
2023-01-17 22:17:01 +0600

    added github button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import { useState } from 'react';
 import './App.css';
 import app from './firebase/firebase.init';
@@ -8,10 +8,11 @@ const auth = getAuth(app);
 function App() {
   const [user, setUser] = useState({});
   
-  const provider = new GoogleAuthProvider();
+  const googleProvider = new GoogleAuthProvider();
+  const gitProvider = new GithubAuthProvider();
 
   const handleGoogleSignIn = () => {
-    signInWithPopup(auth, provider)
+    signInWithPopup(auth, googleProvider)
     .then(result => {
       const user = result.user;
       setUser(user);
@@ -31,16 +32,38 @@ function App() {
       setUser({});
     })
   }
+
+  const handleGitHubSignIn = () => {
+    signInWithPopup(auth, gitProvider)
+    .then(result => {
+      // const credential = GithubAuthProvider.credentialFromResult(result);
+      // const token = credential.accessToken;
+
+      const user = result.user;
+      console.log(user);
+      setUser(user);
+    })
+    .catch(error => {
+      console.error("error: ", error);
+    })
+  }
   
   return (
     <div className="App">
-      <button onClick = {handleGoogleSignIn}>Sign In with Google</button>
-      <button onClick = {handleSignOut}>Sign Out</button>
+      {
+        user.uid ?
+          <button onClick = {handleSignOut}>Sign Out</button>
+          :
+          <>
+          <button onClick = {handleGoogleSignIn}>Sign In with Google</button>
+          <button onClick={handleGitHubSignIn}>Sign In with Github</button>
+          </>
+      }
       
       {/* user info */}
       
       {
-        user.email &&
+        user.uid &&
           <div>
             <h3>User Name: {user.displayName}</h3>
             <p>Email: {user.email}</p>
