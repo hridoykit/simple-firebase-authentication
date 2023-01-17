cedcfd015943b1d300325363d7089b647f7e4a02
hridoykit <hridoy.tsa@gmail.com>
2023-01-17 22:06:44 +0600

    sign out button, display user info

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { useState } from 'react';
 import './App.css';
 import app from './firebase/firebase.init';
 
 const auth = getAuth(app);
 
 function App() {
-
+  const [user, setUser] = useState({});
+  
   const provider = new GoogleAuthProvider();
 
-  const handleSignInBtn = () => {
+  const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
     .then(result => {
       const user = result.user;
+      setUser(user);
       console.log(user);
     })
     .catch(error =>{
       console.error('error: ', error);
     })
   }
+
+  const handleSignOut = () => {
+    signOut(auth)
+    .then(() => {
+      setUser({});
+    })
+    .catch(() => {
+      setUser({});
+    })
+  }
   
   return (
     <div className="App">
-      <button onClick = {handleSignInBtn}>google sign in</button>
+      <button onClick = {handleGoogleSignIn}>Sign In with Google</button>
+      <button onClick = {handleSignOut}>Sign Out</button>
+      
+      {/* user info */}
+      
+      {
+        user.email &&
+          <div>
+            <h3>User Name: {user.displayName}</h3>
+            <p>Email: {user.email}</p>
+            <img src={user.photoURL} alt="" />
+          </div>
+      }
     </div>
   );
 }
